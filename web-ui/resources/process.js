135027485054f86936ba2a0175b2161a3d5db5d0
Sedfik <ramos.val.91@gmail.com>
2020-03-09 22:18:30 +0100

    Pages

--- web-ui/resources/process.js
+++ web-ui/resources/process.js
@@ -4,7 +4,8 @@ function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
+        document.getElementById("nbTweets").innerHTML =  this.responseText.length
+
       }
     };
     xhttp.open("GET", "tweets.json", true);

6cc257ec6df3cb5a545707335fe6c296f18bc87f
Sedfik <ramos.val.91@gmail.com>
2020-03-09 22:14:07 +0100

    Documentation pathparser

--- web-ui/src/pathparser.py
+++ web-ui/src/pathparser.py
@@ -1,10 +1,20 @@
+"""
+Classe representant un Path. Un Path est l'agregation de :
+    - D'une ressource 
+    - De parametres (facultatif)
 
+l'attribut parametre est == None si aucuns parametres.
+Les attributs sont privés
+"""
 class Path():
 
     def __init__(self, path):
         self.__path = path
         self.__resource = path.split("?")[0]
-        self.__parameters = dict((k,v) for k, v in [tuple(p.split("=")) for p in path.split("?")[1].split("&")])
+        try:
+            self.__parameters = dict((k,v) for k, v in [tuple(p.split("=")) for p in path.split("?")[1].split("&")])
+        except:
+            self.__parameters = None
 
     @property
     def path(self):
@@ -18,7 +28,13 @@ class Path():
     def parameters(self):
         return self.__parameters
 
+# Scenario nominal
 path = Path("/index.html?param1=21&param2=4")
 assert(path.path == "/index.html?param1=21&param2=4")
 assert(path.resource == "/index.html")
 assert(path.parameters == dict({'param1': '21', 'param2': '4'}))
+
+# Cas speciaux
+assert(Path("/index?a").parameters == None)
+assert(Path("/index").parameters == None)
+assert(Path("/index?a=").parameters == dict({'a':''}))

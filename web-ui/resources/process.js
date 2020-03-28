4a45da9908b5cf5b43690d799e4d335825a53cc6
Sedfik <ramos.val.91@gmail.com>
2020-03-28 19:07:21 +0100

    Nouvelle interface, systeme de tabs pour naviguer entre les resultats. Creation d'une fonction qui liste tous les tweets ainsi qu'une autre listant tous les tweets avec leurs champs

--- web-ui/resources/process.js
+++ web-ui/resources/process.js
@@ -22,9 +22,99 @@ function loadDoc() {
 
         console.log(res);
         document.getElementById("nbTweets").innerHTML =  this.responseText.length;
-        document.getElementById("brutData").innerHTML = JSON.stringify(res);
+        //document.getElementById("brutData").innerHTML = JSON.stringify(res);
+
+        let listDiv = document.getElementById("tweetList");
+        console.log("list:" + listDiv);
+      
+        //clearDiv(listDiv);
+
+        // Creation du tableau contenant les tweets
+        let table = document.createElement("table");
+        
+        listDiv.appendChild(table)
+
+        // Fonction de remplissage du tableau
+        res.forEach(e => {
+          console.log("Ajout des elements");
+          let tr = document.createElement("tr");
+          table.appendChild(tr)
+
+          let td = document.createElement("td")
+
+          let userName = e["user_name"];
+          let text = e["text"];
+
+          td.innerHTML += "<strong>" + userName + "</strong>"
+                          + "</br>"
+                          + text ;
+          
+          tr.appendChild(td)
+          
+        });
+        console.log(listDiv.innerHTML);
+        
+        // TODO a debuger: Affichage simultane de brutDataDiv et listDiv
+        // let brutDiv = document.getElementById("brutData");
+
+        //brutDiv.appendChild(brutData(res));
+
       }
     };
     xhttp.open("GET", "tweets?"+query, true);
     xhttp.send();
+}
+
+
+/**
+ * Affiche les donnees brut dans un tableau
+ * 
+ * @param {*} jsonData Un fichier Json contenant les donnees 
+ * @returns {*} table: Le noeud DOM contenant un tableau des donnees
+ */
+function brutData(jsonData) {
+
+  // Creation de la table des donnees
+  let table = document.createElement("table");
+
+  // Recuperation des index
+  let tr = document.createElement("tr");
+  table.appendChild(tr);
+
+  for(key in jsonData[0]){
+  
+    let th = document.createElement("th");
+    th.innerHTML = key;
+    tr.appendChild(th);
+  
+  }
+
+  // Remplissage des lignes
+  jsonData.forEach(element => {
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+    for(key in element){
+      let td = document.createElement("td")
+      td.innerHTML = element[key];
+      tr.appendChild(td);
+   }
+  });
+  return table;
+}
+
+/**
+ * Efface tous les noeuds fils d'une div 
+ * 
+ * @param {*} div la div a clear 
+ */
+// TODO A tester / verifier
+function clearDiv(div) {
+  console.log("clear div: "+div);
+  try {
+    while(div.lastElementChild){
+      div.removeChild(listDiv.lastElementChild);
+  } 
+  } catch (error) {
+    console.error(error);
+  }
 }

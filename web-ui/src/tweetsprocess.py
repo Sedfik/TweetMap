8b9de326b1ac428bb321093d22d85a997f500e6e
Sedfik <ramos.val.91@gmail.com>
2020-04-03 11:10:43 +0200

    Suppressions des anciennes fonctions de filtre

--- web-ui/src/tweetsprocess.py
+++ web-ui/src/tweetsprocess.py
@@ -22,35 +22,10 @@ def get_tweets(data_frame):
 # parametre : data_frame -> le jeu de donnees sur lequel appliquer les filtres 
 # parametre : parameters -> la liste des parametres sous forme: {'params1': ['value1'],'params1': ['value1']}
 
-def get_tweets_query(data_frame,parameters):
-    for p in parameters:
-        data_frame = filter(data_frame,(p,parameters[p]))
-    return str(data_frame.to_json(orient="records"))
-
-
-# Retourne le data_frame des lignes qui match avec le parametre.
-def filter(data_frame, parameter):
-    finded = []
-    
-    for t in data_frame[str(parameter[0])]:
-    
-        if(re.search(str(parameter[1]),str(t))):
-            finded.append(True)
-        else:
-            finded.append(False)
-    
-    return data_frame[finded]
-
 """
-
-
 def filter(data_frame, parameter, value):
     patern = re.compile(value)
     return data_frame[data_frame[parameter].str.contains(patern)]
-    #return data_frame[ (patern.search(data_frame["text"]) ) ]
-    #data_frame["text"].apply(lambda x: patern.search(x),1)
-    #filter(lambda x: True if x == "null")
-    #return data_frame[ data_frame [ re.search( str(parameter[1]), str(parameter[0])) ] ]
 
 def get_tweets_query(data_frame,parameters):
     for p in parameters:
@@ -60,11 +35,7 @@ def get_tweets_query(data_frame,parameters):
 
 
 """
-
 [['user_id','user_name','user_screen_name','user_followers_count']]
 """
 if __name__ == "__main__":
-    #print(get_tweets(get_file_dataframe("web-ui/resources/twets.csv")))
-    #print(new_filter(get_file_dataframe(config.ROOT_DIR + "/web-ui/test/test_tweets.csv"),""))
-    #print(filter(get_file_dataframe(config.ROOT_DIR + "web-ui/test/test_tweets.csv"),("text","dog")))
     print(get_tweets_query(get_file_dataframe(config.ROOT_DIR + "/web-ui/test/test_tweets.csv"), dict({'text': ['dog']} )))

167054b8d1270cb8c430c95b0adcbc9ce164d5d7
Sedfik <ramos.val.91@gmail.com>
2020-04-09 11:14:11 +0200

    Modification de la requette de test

--- web-ui/src/RequestSpeed.py
+++ web-ui/src/RequestSpeed.py
@@ -2,7 +2,7 @@ import requests
 from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 
 
-links = ['http://localhost:8800/tweets?text=']*500
+links = ['http://localhost:8800/tweets?text=dog']*500
 
 
 with PoolExecutor(max_workers=20) as executor:
